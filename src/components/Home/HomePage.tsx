f1983145a9e11863bb4c142cfd1f6a81bf9f876c
Kazuki Nishiura <kazuki@sardine.ai>
2025-01-24 12:12:20 -0800

    use radix

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,5 +1,31 @@
 import { FC } from 'react';
 import ReactTypescriptTemplateLogo from '../../assets/images/react-typescript-template.png';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+
+console.log(i18n, initReactI18next);
+
+export interface BreadcrumbItems {
+  label: string;
+  href?: string;
+  dataTestId?: string;
+}
+
+const OverflowedDropdown = ({ items }: { items: BreadcrumbItems[] }) => (
+  <DropdownMenu.Root modal={false}>
+    <DropdownMenu.Trigger asChild>...</DropdownMenu.Trigger>
+    <DropdownMenu.Portal>
+      <DropdownMenu.Content asChild sideOffset={10} align="start">
+        {items.map((item) => (
+          <DropdownMenu.Item asChild key={JSON.stringify(item)}>
+            {item.label}
+          </DropdownMenu.Item>
+        ))}
+      </DropdownMenu.Content>
+    </DropdownMenu.Portal>
+  </DropdownMenu.Root>
+);
 
 const HomePage: FC = () => {
   return (
@@ -11,6 +37,7 @@ const HomePage: FC = () => {
         className="mx-auto"
         alt="React-Typescript-Template"
       />
+      <OverflowedDropdown items={[{ label: 'hello' }]} />
     </div>
   );
 };
